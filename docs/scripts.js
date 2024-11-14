c0d8d3e575f867709d1e704a9c2fc004faadef8e
SeverusSnape799 <79190643+SeverusSnape799@users.noreply.github.com>
2024-11-14 22:03:20 +0530

    Add files via upload

---
+++ docs/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger-menu").addEventListener("click", function() {
+    const navbarLinks = document.getElementById("navbar-links");
+    navbarLinks.classList.toggle("open");
+});
